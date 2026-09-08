12417fed29c769d9f3bcadba230d75592be8dfa3
mahamood-1322 <syedmahamood888@gmail.com>
2026-09-08 13:23:12 +0530

    all four task about: sum,square,perimeter,temperature

---
+++ task1(addition).py
@@ -0,0 +1,3 @@
+first_num=int(input("Enter the first number: "))
+second_num=int(input("Enter the second number: "))
+print("Sum:",first_num+second_num)
---
+++ task3(perimeter).py
@@ -0,0 +1,6 @@
+length=int(input("Enter the length:"))
+width=int(input("Enter the width:"))
+area=length*width
+perimeter=2*(length+width)
+print("Area:",area)
+print("Perimeter:",perimeter)
