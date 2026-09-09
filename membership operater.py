96b33210a5bfa5c18b1e8e9e4843d8763c694f50
mahamood-1322 <syedmahamood888@gmail.com>
2026-09-09 11:54:23 +0530

    operators

---
+++ membership operater.py
@@ -0,0 +1,11 @@
+a= 1 in [1,2,3]
+print("result for in operator1:",a)
+
+b= 4 in [1,2,3]
+print("result for in operator2:",b)
+
+c= 3 not  in[4,5,6] 
+print("result for not in operator3:",c)
+
+d= 4 not in [4,5,6]
+print("result for not in operator4:",d)
