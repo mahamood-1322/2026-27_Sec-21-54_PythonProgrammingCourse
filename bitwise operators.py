bb5cae56ea74849a28374e6b79169c7c0447ff05
mahamood-1322 <syedmahamood888@gmail.com>
2026-09-09 11:53:17 +0530

    operators

---
+++ bitwise operators.py
@@ -0,0 +1,11 @@
+a = 10 
+b = 4    
+print("a =", a)
+print("b =", b)
+print("a & b =", a & b)   
+print("a | b =", a | b)   
+print("a ^ b =", a ^ b)   
+print("~a =", ~a)         
+print("a << 1 =", a << 1) 
+print("a >> 1 =", a >> 1) 
+
---
+++ identity operators.py
@@ -0,0 +1,19 @@
+a=[1,2,3]
+b=a
+result=b is a
+print("result for is operator:",result)
+
+c=[1,2,3]   
+d=[1,2,3]
+result2=c is d
+print("result2 for is operator:",result2)
+
+a=[4,5,6]
+b=a
+result3=b is not a
+print("result3 for is not operator:",result3)
+
+c=[6,7,8]
+d=[6,7,8]
+result4=c is not d
+print("result4 for is not operator:",result4)
